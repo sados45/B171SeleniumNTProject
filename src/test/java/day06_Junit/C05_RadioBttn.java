cb6028efd0c8a2e4e576ec638daca0bf69c8f6fa
sedat <somamebarge@gmail.com>
2023-08-19 17:39:37 +0200

    RadioButton

--- src/test/java/day06_Junit/C05_RadioBttn.java
+++ src/test/java/day06_Junit/C05_RadioBttn.java
@@ -25,7 +25,7 @@ public class C05_RadioBttn {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.get("https://www.facebook.com");
+        driver.get("https://www.facebook.com.");
         driver.findElement(By.xpath("(//[@Title='Tüm cerezlere izin ver']) ")).click();
     }
 
