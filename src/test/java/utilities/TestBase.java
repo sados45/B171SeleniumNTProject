0f3d132781862a8437d4e791f9e7c3c6cb72decf
sedat <somamebarge@gmail.com>
2023-08-26 15:49:46 +0200

    ScreenShot

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,7 +1,11 @@
 package utilities;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +14,11 @@ import org.openqa.selenium.support.ui.Select;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public abstract class TestBase {
     protected WebDriver driver;
@@ -58,7 +66,6 @@ public abstract class TestBase {
     public void frameIndex(int index){
         driver.switchTo().frame(index);
     }
-
     //UploadFile Robot Class
     public void uploadFilePath(String dosyaYolu) {
         try {
@@ -81,7 +88,29 @@ public abstract class TestBase {
         } catch (AWTException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    //ScreenShot
+    public void ekranResmi() {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "target/ekranGoruntusu/screenShot"+tarih+".png";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        try {
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //WebElement ScreenShot
+    public void webElementResmi(WebElement element){
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "target/webElementEkranGoruntusu/WEscreenShot"+tarih+".png";
+        try {
+            FileUtils.copyFile(element.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
